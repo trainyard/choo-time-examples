cbe20b351b1d2ecf909f4628c1f323e199975c31
Matt McFarland <contact@mattmcfarland.com>
2016-12-01 17:07:52 -0500

    tolower on searches

--- pages/search.js
+++ pages/search.js
@@ -8,13 +8,14 @@ function search (state, prev, send) {
     <main>
       ${navigation()}
       <h1>search</h1>
-      <input type="text" 
+      <input type="text"
+             autofocus 
              placeholder="search"
              value=${state.search.criteria}
              oninput=${e => send('search:update', e.target.value)} />
      <ul>
       ${state.search.searchList
-          .filter(item => item.indexOf(state.search.criteria) > -1 )
+          .filter(item => item.toLowerCase().indexOf(state.search.criteria.toLowerCase()) > -1 )
           .map(item => html`<li>${item}</li>`
       )}
      </ul>
